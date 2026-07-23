ab623e180acdd17748735ac3ef2c60922b280910
Vishay Raina <46987787+vishayraina@users.noreply.github.com>
2026-07-23 17:39:25 +0530

    Time: 37 ms (52.34%), Space: 31.4 MB (73.61%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,6 +1,5 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        l = 0
         res = -float("inf")
         curr = 0 
         for r in range(len(nums)):
@@ -8,7 +7,6 @@ class Solution:
             res = max(curr, res)
             if curr < 0:
                 curr = 0
-                l = r+1
 
         return res
             
