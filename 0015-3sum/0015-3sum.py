5d71a58dde54816f2bd94f6327e0ab3c956138e7
Vishay Raina <46987787+vishayraina@users.noreply.github.com>
2026-07-23 16:28:10 +0530

    Time: 513 ms (84.72%), Space: 22.7 MB (14.54%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,24 +1,26 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-        def twoSum(arr, target, i, j):
+        nums.sort()
+        def twoSum(nums, l, r, target):
             res = []
-            while i < j:
-                if arr[i] + arr[j] == target:
-                     res.append([arr[i], arr[j]])
-                     i += 1
-                     j -= 1
-                elif arr[i] + arr[j] > target:
-                    j -= 1
+            while l < r:
+                if nums[l] + nums[r] > target:
+                    r -= 1
+                elif nums[l] + nums[r] < target:
+                    l += 1
                 else:
-                    i += 1
+                    res.append([-target, nums[l], nums[r]])
+                    l += 1 
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
+                    r -= 1
+                    while l < r and nums[r] == nums[r+1]:
+                        r -= 1
             return res
-        
-        nums = sorted(nums)
-        res = set()
-        for i in range(len(nums)):
-            ts = twoSum(nums, -nums[i], i+1, len(nums)-1)
-            for x in ts: 
-                x.append(nums[i])
-                res.add(tuple(x))
-        return [list(t) for t in res]
-        
+        res = []
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            tsum = twoSum(nums, i+1, len(nums)-1, -nums[i])
+            res.extend(tsum)
+        return res
